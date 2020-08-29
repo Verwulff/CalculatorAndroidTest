8cc3b0b6d7de06afe27454b8a0a66b45a7279fa7
Verwulf <verowulf@gmail.com>
2020-08-29 23:43:12 +0300

    Commit 2: add first version off = listener

--- app/src/main/java/com/example/calculatortest/Checker.java
+++ app/src/main/java/com/example/calculatortest/Checker.java
@@ -9,13 +9,24 @@ public class Checker {
     public static final String OPEN_PARENTHESES = "(";
     public static final String CLOSE_PARENTHESES = ")";
     public static final String EQUAL = "=";
+    public static final String DOTE = ".";
+    //должно быть 25, потом поправлю
+    public static final int MAX_NUMBER_LENGTH = 5;
 
     public static boolean isNumber(String s)
     {
-        if (s.equals(PLUS) || s.equals(MINUS) || s.equals(MULTIPLY) || s.equals(DIVISION)
+        if (s.equals(PLUS) || s.equals(MINUS) || s.equals(MULTIPLY) || s.equals(DIVISION) || s.equals(DOTE)
             || s.equals(OPEN_PARENTHESES) || s.equals(CLOSE_PARENTHESES) || s.equals(EQUAL))
             return false;
         else
             return true;
     }
+
+    public static boolean outOfDigits(String s) {
+        if (s.length()==MAX_NUMBER_LENGTH)
+            return false;
+        else
+            return true;
+    }
+
 }
--- app/src/main/java/com/example/calculatortest/MainActivity.java
+++ app/src/main/java/com/example/calculatortest/MainActivity.java
@@ -8,12 +8,14 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -22,13 +24,20 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     TextView resultField;
-    EditText calcField;
+    TextView calcField;
+    Button equalButton;
     String lastDigit = "="; // последняя операция
     List<Calculator> calc;
     StringBuilder currentNumber;
     int depths = 0;
     int parenthesesOpen=0;
     int parenthesesClose=0;
+    //переменные для обработки всплывающего меню
+    long equalPressStartTime;
+    long equalPressEndTime;
+    long onScreenTouchTimeFirst;
+    long onScreenTouchTimeSecond;
+    boolean menuCorrectTime = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +45,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         resultField =(TextView) findViewById(R.id.resultField);
-        calcField =(EditText) findViewById(R.id.calcField);
+        calcField =(TextView) findViewById(R.id.calcField);
+        equalButton = (Button) findViewById(R.id.equalButton);
         calc = new LinkedList<Calculator>();
         calc.add(new Calculator());
         currentNumber = new StringBuilder();
+
+        calcField.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float x = event.getX();
+                float y = event.getY();
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        onScreenTouchTimeFirst = System.currentTimeMillis();
+                        Long check = (onScreenTouchTimeFirst - equalPressEndTime)/1000;
+                        if(check<5)
+                        {
+                            showMessage("Успел");
+                        }
+                        resultField.append(check.toString());
+                        break;
+                }
+                return false;
+            }
+        });
+
+        equalButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        equalPressStartTime = System.currentTimeMillis();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        equalPressEndTime = System.currentTimeMillis();
+                        Long check = (equalPressEndTime-equalPressStartTime)/1000;
+                        if (check>=4)
+                        {
+                            menuCorrectTime = true;
+                            showMessage("Прикинь, работает");
+                        }
+                        resultField.append(check.toString());
+                        calc.get(depths).result();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     // обработка нажатия на числовую кнопку
@@ -47,8 +102,20 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = (Button)view;
         String digit = button.getText().toString();
-        if ((digit == ".") && (!Checker.isNumber(lastDigit)))
+        if (!Checker.outOfDigits(currentNumber.toString())) {
+            showMessage("Число не может быть больше 25 знаков");
             return;
+        }
+        if (digit.equals(".")){
+            if (!Checker.isNumber(lastDigit)){
+                showMessage("Сперва введите число");
+                return;
+            }
+            if (currentNumber.toString().contains(".")){
+                showMessage("Не больше одного разделителя в числе");
+                return;
+            }
+        }
         lastDigit = digit;
         currentNumber.append(digit);
         calcField.append(digit);
@@ -77,23 +144,27 @@ public class MainActivity extends AppCompatActivity {
             case "+":
                 calc.get(depths).numbers.add(Double.parseDouble(currentNumber.toString()));
                 calc.get(depths).operations.add(s);
+                currentNumber.setLength(0);
                 calcField.append(s);
                 break;
             case "-":
                 calc.get(depths).numbers.add(Double.parseDouble(currentNumber.toString()));
                 calc.get(depths).operations.add(s);
+                currentNumber.setLength(0);
                 calcField.append(s);
                 break;
             case "×":
                 s = "*";
                 calc.get(depths).numbers.add(Double.parseDouble(currentNumber.toString()));
                 calc.get(depths).operations.add(s);
+                currentNumber.setLength(0);
                 calcField.append(s);
                 break;
             case "÷":
                 s="/";
                 calc.get(depths).numbers.add(Double.parseDouble(currentNumber.toString()));
                 calc.get(depths).operations.add(s);
+                currentNumber.setLength(0);
                 calcField.append(s);
                 break;
             default:
@@ -102,5 +173,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void showMessage(String message)
+    {
+        Toast toast = Toast.makeText(this, message,Toast.LENGTH_LONG);
+        toast.show();
+    }
 
 }
